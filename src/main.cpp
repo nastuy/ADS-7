de150e2c855a06740900bd4e5ce8b8ef2b6c86f2
nastuy <nastuysandalova@gmail.com>
2025-05-16 20:21:55 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -12,24 +12,25 @@ int main() {
   for (int n = 500; n <= 10000; n += 500) {
     train_lengths.push_back(n);
   }
-  std::cout << "Length | All off | Time off | All on | Time on | Random | Time random\n";
+  std::cout << "Length | All off | Time off | All on |"
+    << "Time on | Random | Time random\n";
   for (int n : train_lengths) {
     Train train_off;
     for (int i = 0; i < n; ++i) train_off.addCar(false);
     double start_off = getCurrentTime();
-    int ops_off = train_off.getLength();
+    train_off.getLength();
     double time_off = getCurrentTime() - start_off;
     int count_off = train_off.getOpCount();
     Train train_on;
     for (int i = 0; i < n; ++i) train_on.addCar(true);
     double start_on = getCurrentTime();
-    int ops_on = train_on.getLength();
+    train_on.getLength();
     double time_on = getCurrentTime() - start_on;
     int count_on = train_on.getOpCount();
     Train train_rand;
     for (int i = 0; i < n; ++i) train_rand.addCar(std::rand() % 2);
     double start_rand = getCurrentTime();
-    int ops_rand = train_rand.getLength();
+    train_rand.getLength();
     double time_rand = getCurrentTime() - start_rand;
     int count_rand = train_rand.getOpCount();
     std::cout << n << "\t" << count_off << "\t" << time_off << " ms\t"
